9addf83d02da438cc3bd27f21a48a62b7e04c014
Morten Stenberdt Espensen <tribute.e@gmail.com>
2018-07-13 08:38:08 +0200

    Add support for parsing whole file structures to POT

--- index.js
+++ index.js
@@ -1,46 +1,50 @@
-'use strict';
+'use strict'
 
-var Handlebars = require('handlebars');
+var Handlebars = require('handlebars')
+var pofile = require('pofile')
+var glob = require('glob')
+var fs = require('fs')
+
 
 function Parser (keywordSpec) {
   // make new optional
   if (!(this instanceof Parser)) {
-    return new Parser(keywordSpec);
+    return new Parser(keywordSpec)
   }
 
-  keywordSpec = keywordSpec || Parser.keywordSpec;
+  keywordSpec = keywordSpec || Parser.keywordSpec
 
   Object.keys(keywordSpec).forEach(function (keyword) {
-    var positions = keywordSpec[keyword];
+    var positions = keywordSpec[keyword]
 
     if ('msgid' in positions) {
-      return;
+      return
     } else if (Array.isArray(positions) && positions.indexOf('msgid') >= 0) {
       // maintain backwards compatibility with `_: ['msgid']` format
       keywordSpec[keyword] = positions.reduce(function (result, key, idx) {
-        result[key] = idx;
+        result[key] = idx
 
-        return result;
-      }, {});
+        return result
+      }, {})
     } else if (Array.isArray(positions) && positions.length > 0) {
       // maintain backwards compatibility with `_: [0]` format
-      var order = ['msgid', 'msgid_plural'];
+      var order = ['msgid', 'msgid_plural']
 
       keywordSpec[keyword] = positions.slice(0).reduce(function (result, pos, idx) {
-        result[order[idx]] = pos;
+        result[order[idx]] = pos
 
-        return result;
-      }, {});
+        return result
+      }, {})
     }
-  });
+  })
 
   Object.keys(keywordSpec).forEach(function (keyword) {
     if (!('msgid' in keywordSpec[keyword])) {
-      throw new Error('Every keyword must have a msgid key, but "' + keyword + '" doesn\'t have one');
+      throw new Error('Every keyword must have a msgid key, but "' + keyword + '" doesn\'t have one')
     }
-  });
+  })
 
-  this.keywordSpec = keywordSpec;
+  this.keywordSpec = keywordSpec
 }
 
 // default keywords, copied from GNU xgettext's JavaScript keywords
@@ -73,125 +77,225 @@ Parser.keywordSpec = {
     msgctxt: 1,
     msgid: 2
   }
-};
+}
 
 // Same as what Jed.js uses
-Parser.contextDelimiter = String.fromCharCode(4);
+Parser.contextDelimiter = String.fromCharCode(4)
 
 Parser.messageToKey = function (msgid, msgctxt) {
-  return msgctxt ? msgctxt + Parser.contextDelimiter + msgid : msgid;
-};
+  return msgctxt ? msgctxt + Parser.contextDelimiter + msgid : msgid
+}
 
 /**
  * Given a Handlebars template string returns the list of i18n strings.
  *
- * @param String template The content of a HBS template.
- * @return Object The list of translatable strings, the line(s) on which each appears and an optional plural form.
+ * @param {String} template The content of a HBS template.
+ * @return {Object} The list of translatable strings, the line(s) on which each appears and an optional plural form.
  */
 Parser.prototype.parse = function (template) {
   var keywordSpec = this.keywordSpec,
     keywords = Object.keys(keywordSpec),
-    tree = Handlebars.parse(template);
+    tree = Handlebars.parse(template)
 
   var isMsg = function (msgs, statement) {
     switch (statement.type) {
     case 'MustacheStatement':
     case 'SubExpression':
       if (keywords.indexOf(statement.path.original) !== -1) {
-        var spec = keywordSpec[statement.path.original];
-        var params = statement.params;
-        var msgidParam = params[spec.msgid];
+        var spec = keywordSpec[statement.path.original]
+        var params = statement.params
+        var msgidParam = params[spec.msgid]
 
         if (msgidParam) { // don't extract {{gettext}} without param
-          var msgid = msgidParam.original;
-          var contextIndex = spec.msgctxt;
-          var context = null; // null context is *not* the same as empty context
+          var msgid = msgidParam.original
+          var contextIndex = spec.msgctxt
+          var context = null // null context is *not* the same as empty context
 
           if (contextIndex !== undefined) {
-            var contextParam = params[contextIndex];
+            var contextParam = params[contextIndex]
 
             if (!contextParam) {
               // throw an error if there's supposed to be a context but not enough
               // parameters were passed to the handlebars helper
-              throw new Error('No context specified for msgid "' + msgid + '"');
+              throw new Error('No context specified for msgid "' + msgid + '"')
             }
 
             if (contextParam.type !== 'StringLiteral') {
-              throw new Error('Context must be a string literal for msgid "' + msgid + '"');
+              throw new Error('Context must be a string literal for msgid "' + msgid + '"')
             }
 
-            context = contextParam.original;
+            context = contextParam.original
           }
 
-          var key = Parser.messageToKey(msgid, context);
-          msgs[key] = msgs[key] || {line: []};
+          var key = Parser.messageToKey(msgid, context)
+          msgs[key] = msgs[key] || {line: []}
 
           // make sure plural forms match
-          var pluralIndex = spec.msgid_plural;
+          var pluralIndex = spec.msgid_plural
           if (pluralIndex !== undefined) {
-            var pluralParam = params[pluralIndex];
+            var pluralParam = params[pluralIndex]
 
             if (!pluralParam) {
-              throw new Error('No plural specified for msgid "' + msgid + '"');
+              throw new Error('No plural specified for msgid "' + msgid + '"')
             }
 
             if (pluralParam.type !== 'StringLiteral') {
-              throw new Error('Plural must be a string literal for msgid ' + msgid);
+              throw new Error('Plural must be a string literal for msgid ' + msgid)
             }
 
-            var plural = pluralParam.original;
-            var existingPlural = msgs[key].msgid_plural;
+            var plural = pluralParam.original
+            var existingPlural = msgs[key].msgid_plural
             if (plural && existingPlural && existingPlural !== plural) {
               throw new Error('Incompatible plural definitions for msgid "' + msgid +
-                '" ("' + msgs[key].msgid_plural + '" and "' + plural + '")');
+                '" ("' + msgs[key].msgid_plural + '" and "' + plural + '")')
             }
           }
 
-          msgs[key].line.push(statement.loc.start.line);
+          msgs[key].line.push(statement.loc.start.line)
 
           Object.keys(spec).forEach(function(prop) {
-            var param = params[spec[prop]];
+            var param = params[spec[prop]]
 
             if (param && param.type === 'StringLiteral') {
-              msgs[key][prop] = params[spec[prop]].original;
+              msgs[key][prop] = params[spec[prop]].original
             }
-          });
+          })
 
           // maintain backwards compatibility with plural output
-          msgs[key].plural = msgs[key].msgid_plural;
+          msgs[key].plural = msgs[key].msgid_plural
         }
       }
 
-      break;
+      break
     case 'BlockStatement':
       if (statement.program) {
-        statement.program.body.reduce(isMsg, msgs);
+        statement.program.body.reduce(isMsg, msgs)
       }
 
       if (statement.inverse) {
-        statement.inverse.body.reduce(isMsg, msgs);
+        statement.inverse.body.reduce(isMsg, msgs)
       }
 
-      break;
+      break
     }
 
     // subexpressions as params
     if (statement.params) {
-      statement.params.reduce(isMsg, msgs);
+      statement.params.reduce(isMsg, msgs)
     }
 
     // subexpressions as hash
     if (statement.hash) {
       statement.hash.pairs.reduce(function (msgs, pair) {
-        return isMsg(msgs, pair.value);
-      }, msgs);
+        return isMsg(msgs, pair.value)
+      }, msgs)
     }
 
-    return msgs;
-  };
+    return msgs
+  }
 
 
-  return tree.body.reduce(isMsg, {});
-};
+  return tree.body.reduce(isMsg, {})
+}
 
-module.exports = Parser;
+/**
+ * Given a parser output generates a corresponding pot output
+ *
+ * @param {String} template The content of a HBS template.
+ * @param {Object} [options={}] Options to set for example the sourcePath
+ * @return {Object[]} The POT file string contents
+ */
+Parser.prototype.parseToPoMessages = function(template, options){
+  if(typeof options !== 'object') {
+    options = {}
+  }
+
+  var parserOutput = this.parse(template)
+
+  var msgids = Object.keys(parserOutput)
+
+  var messages = []
+
+  for(var i=0; i<msgids.length; i++) {
+    var msgid = msgids[i]
+    var msgdetails = parserOutput[msgid]
+
+    var poitem = new pofile.Item()
+
+    poitem.msgid = msgid
+
+    if(msgdetails.plural !== undefined) {
+      poitem.msgid_plural = msgdetails.plural
+    }
+
+    if(options.sourcePath !== undefined) {
+      poitem.references = msgdetails.line.map(function(line) { return options.sourcePath + ':' + line })
+    }
+    messages.push(poitem)
+  }
+
+
+  return messages
+}
+
+/**
+ * Given a list of PO message objects‚ returns a corresponding POT file content string
+ *
+ * @param {Object[]} messages PO messages to compile to a POT file
+ * @return String The POT file string contents
+ */
+Parser.prototype.messagesToPot = function(messages) {
+  var output = new pofile()
+  output.headers = {
+    'Content-Type': 'text/plain; charset=UTF-8'
+  }
+  output.items = messages
+
+  return output.toString()
+}
+
+
+/**
+ * Given a list of PO message objects‚ returns a corresponding POT file content string
+ *
+ * @param {String} pattern Glob targeting files to parse
+ * @param {Object} options PO messages to compile to a POT file
+ * @param {Function} cb callback invoked with the output message of type, with the signature (er, result) => {...}
+ */
+Parser.prototype.parseFilesGlob = function(pattern, options, cb) {
+  var parserInstance = this
+
+  glob(pattern, options, function(er, files) {
+    if(er) {
+      cb(er, null)
+    }
+
+    var messages = {}
+
+    files.map(function(file) {
+      var contents = fs.readFileSync(file).toString()
+      var fileMessages = parserInstance.parseToPoMessages(contents, {sourcePath: file})
+      
+      fileMessages.map(function(message) {
+        if(messages[message.msgid] === undefined) {
+          messages[message.msgid] = message
+        } else {
+          // Join references and sort them
+          messages[message.msgid].references = messages[message.msgid].references.concat(message.references).sort(function(a,b) { return a.localeCompare(b)})
+        }
+      })
+    })
+
+    var output = []
+
+    Object.keys(messages).map(function(msgid) {
+      output.push(messages[msgid])
+    })
+
+    cb(null, output)
+  })
+}
+
+
+
+module.exports = Parser
--- test/index.js
+++ test/index.js
@@ -1,128 +1,128 @@
-'use strict';
+'use strict'
 
 var Parser = require('..'),
   fs = require('fs'),
-  assert = require('assert');
+  assert = require('assert')
 
 describe('Parser', function () {
   describe('#()', function () {
     it('should have default keyword spec when none is passed', function () {
-      assert(Object.keys((new Parser()).keywordSpec.gettext).length > 0);
-    });
+      assert(Object.keys((new Parser()).keywordSpec.gettext).length > 0)
+    })
 
     it('should convert old spec formats', function () {
-      assert.deepEqual((new Parser({_: [0]})).keywordSpec, {_: {msgid: 0}});
-      assert.deepEqual((new Parser({n_: [0, 1]})).keywordSpec, {n_: {msgid: 0, msgid_plural: 1}});
+      assert.deepEqual((new Parser({ _: [0] })).keywordSpec, { _: { msgid: 0 } })
+      assert.deepEqual((new Parser({ n_: [0, 1] })).keywordSpec, { n_: { msgid: 0, msgid_plural: 1 } })
 
-      var spec = new Parser({n_: [2, 1]}).keywordSpec.n_;
-      assert.equal(Object.keys(spec).length, 2);
-      assert.equal(spec.msgid_plural, 1);
-      assert.equal(spec.msgid, 2);
+      var spec = new Parser({ n_: [2, 1] }).keywordSpec.n_
+      assert.equal(Object.keys(spec).length, 2)
+      assert.equal(spec.msgid_plural, 1)
+      assert.equal(spec.msgid, 2)
 
-      spec = new Parser({n_: [1, 2]}).keywordSpec.n_;
-      assert.equal(Object.keys(spec).length, 2);
-      assert.equal(spec.msgid, 1);
-      assert.equal(spec.msgid_plural, 2);
+      spec = new Parser({ n_: [1, 2] }).keywordSpec.n_
+      assert.equal(Object.keys(spec).length, 2)
+      assert.equal(spec.msgid, 1)
+      assert.equal(spec.msgid_plural, 2)
 
-      spec = new Parser({ngettext: ['msgid', 'msgid_plural']}).keywordSpec.ngettext;
-      assert.equal(Object.keys(spec).length, 2);
-      assert.equal(spec.msgid, 0);
-      assert.equal(spec.msgid_plural, 1);
-    });
-  });
+      spec = new Parser({ ngettext: ['msgid', 'msgid_plural'] }).keywordSpec.ngettext
+      assert.equal(Object.keys(spec).length, 2)
+      assert.equal(spec.msgid, 0)
+      assert.equal(spec.msgid_plural, 1)
+    })
+  })
 
   describe('#parse()', function () {
     it('should return results', function (done) {
-      fs.readFile(__dirname + '/fixtures/template.hbs', {encoding: 'utf8'}, function (err, data) {
+      fs.readFile(__dirname + '/fixtures/template.hbs', { encoding: 'utf8' }, function (err, data) {
         if (err) {
-          throw err;
+          throw err
         }
 
-        var result = (new Parser()).parse(data);
+        var result = (new Parser()).parse(data)
 
-        assert.equal(typeof result, 'object');
-        assert('inside block' in result);
-        assert('inside block inverse' in result);
-        assert.equal(Object.keys(result).length, 8);
-        assert.equal(result['Image description'].line.length, 2);
+        assert.equal(typeof result, 'object')
+        assert('inside block' in result)
+        assert('inside block inverse' in result)
+        assert.equal(Object.keys(result).length, 8)
+        assert.equal(result['Image description'].line.length, 2)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('should return plural results', function (done) {
-      fs.readFile(__dirname + '/fixtures/plural.hbs', {encoding: 'utf8'}, function (err, data) {
+      fs.readFile(__dirname + '/fixtures/plural.hbs', { encoding: 'utf8' }, function (err, data) {
         if (err) {
-          throw err;
+          throw err
         }
 
-        var result = (new Parser()).parse(data);
+        var result = (new Parser()).parse(data)
 
-        assert.equal(Object.keys(result).length, 2);
-        assert.equal(result['default'].plural, 'defaults');
+        assert.equal(Object.keys(result).length, 2)
+        assert.equal(result['default'].plural, 'defaults')
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('should throw an error if there are mismatched plurals', function (done) {
-      fs.readFile(__dirname + '/fixtures/mismatched-plurals.hbs', {encoding: 'utf8'}, function (err, data) {
+      fs.readFile(__dirname + '/fixtures/mismatched-plurals.hbs', { encoding: 'utf8' }, function (err, data) {
         if (err) {
-          throw err;
+          throw err
         }
 
-        assert.throws(function() { new Parser().parse(data); }, Error);
+        assert.throws(function () { new Parser().parse(data) }, Error)
 
-        done();
-      });
-    });
+        done()
+      })
+    })
 
     it('should recognize subexpressions', function (done) {
-      fs.readFile(__dirname + '/fixtures/subexpression.hbs', {encoding: 'utf8'}, function (err, data) {
+      fs.readFile(__dirname + '/fixtures/subexpression.hbs', { encoding: 'utf8' }, function (err, data) {
         if (err) {
-          throw err;
+          throw err
         }
 
-        var result = (new Parser()).parse(data);
+        var result = (new Parser()).parse(data)
 
-        assert('subexpression' in result);
-        assert('%s subexpression' in result);
-        assert.equal(result['%s subexpression'].plural, '%s subexpressions');
-        assert('%s %s subexpression' in result);
-        assert.equal(result['%s %s subexpression'].plural, '%s %s subexpressions');
-        assert('second' in result);
-        assert('regular' in result);
-        assert('%s %s other' in result);
-        assert('nested %s' in result);
-        assert('dummy_hash_text' in result);
-        assert('dummy_hash_text_only' in result);
-        assert('subexpression_from_from_partial' in result);
-        assert.equal(10, Object.keys(result).length);
+        assert('subexpression' in result)
+        assert('%s subexpression' in result)
+        assert.equal(result['%s subexpression'].plural, '%s subexpressions')
+        assert('%s %s subexpression' in result)
+        assert.equal(result['%s %s subexpression'].plural, '%s %s subexpressions')
+        assert('second' in result)
+        assert('regular' in result)
+        assert('%s %s other' in result)
+        assert('nested %s' in result)
+        assert('dummy_hash_text' in result)
+        assert('dummy_hash_text_only' in result)
+        assert('subexpression_from_from_partial' in result)
+        assert.equal(10, Object.keys(result).length)
 
-        done();
-      });
-    });
-  });
+        done()
+      })
+    })
+  })
 
   it('should support skipping parameters', function (done) {
-    fs.readFile(__dirname + '/fixtures/skip-params.hbs', {encoding: 'utf8'}, function (err, data) {
+    fs.readFile(__dirname + '/fixtures/skip-params.hbs', { encoding: 'utf8' }, function (err, data) {
       if (err) {
-        throw err;
+        throw err
       }
 
-      var result = new Parser({_: [1, 2]}).parse(data);
+      var result = new Parser({ _: [1, 2] }).parse(data)
 
-      assert.equal(result.msgid.msgid, 'msgid');
-      assert.equal(result.msgid.msgid_plural, 'plural');
+      assert.equal(result.msgid.msgid, 'msgid')
+      assert.equal(result.msgid.msgid_plural, 'plural')
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
   it('should support extracting contexts', function (done) {
-    fs.readFile(__dirname + '/fixtures/contexts.hbs', {encoding: 'utf8'}, function (err, data) {
+    fs.readFile(__dirname + '/fixtures/contexts.hbs', { encoding: 'utf8' }, function (err, data) {
       if (err) {
-        throw err;
+        throw err
       }
 
       var result = (new Parser({
@@ -135,46 +135,173 @@ describe('Parser', function () {
           msgid: 1,
           msgid_plural: 2
         }
-      })).parse(data);
+      })).parse(data)
 
-      var key = Parser.messageToKey('msgid', 'first context');
-      assert(key in result);
-      assert.equal(result[key].msgctxt, 'first context');
+      var key = Parser.messageToKey('msgid', 'first context')
+      assert(key in result)
+      assert.equal(result[key].msgctxt, 'first context')
 
-      key = Parser.messageToKey('msgid', 'second context');
-      assert(key in result);
-      assert.equal(result[key].msgctxt, 'second context');
+      key = Parser.messageToKey('msgid', 'second context')
+      assert(key in result)
+      assert.equal(result[key].msgctxt, 'second context')
 
-      key = Parser.messageToKey('file', 'first context');
-      assert(key in result);
-      assert.equal(result[key].msgctxt, 'first context');
-      assert.equal(result[key].msgid_plural, 'files');
-      assert.equal(result[key].plural, 'files');
+      key = Parser.messageToKey('file', 'first context')
+      assert(key in result)
+      assert.equal(result[key].msgctxt, 'first context')
+      assert.equal(result[key].msgid_plural, 'files')
+      assert.equal(result[key].plural, 'files')
 
-      key = Parser.messageToKey('file', 'second context');
-      assert(key in result);
-      assert.equal(result[key].msgctxt, 'second context');
-      assert.equal(result[key].msgid_plural, 'files');
-      assert.equal(result[key].plural, 'files');
+      key = Parser.messageToKey('file', 'second context')
+      assert(key in result)
+      assert.equal(result[key].msgctxt, 'second context')
+      assert.equal(result[key].msgid_plural, 'files')
+      assert.equal(result[key].plural, 'files')
 
-      assert.equal(4, Object.keys(result).length);
+      assert.equal(4, Object.keys(result).length)
 
-      done();
-    });
-  });
+      done()
+    })
+  })
 
   it('should support being called without `new`', function (done) {
     /* jshint newcap: false */
-    fs.readFile(__dirname + '/fixtures/template.hbs', {encoding: 'utf8'}, function (err, data) {
+    fs.readFile(__dirname + '/fixtures/template.hbs', { encoding: 'utf8' }, function (err, data) {
       if (err) {
-        throw err;
+        throw err
       }
 
-      var result = Parser().parse(data);
+      var result = Parser().parse(data)
 
-      assert('inside block' in result);
+      assert('inside block' in result)
 
-      done();
-    });
-  });
-});
+      done()
+    })
+  })
+
+  describe('#parseToPoMessages()', function () {
+    it('should return results', function (done) {
+      fs.readFile(__dirname + '/fixtures/template.hbs', { encoding: 'utf8' }, function (err, data) {
+        if (err) {
+          throw err
+        }
+
+        var result = (new Parser()).parseToPoMessages(data)
+        assert.equal(result.length, 8)
+
+        var expected = [{
+          "msgid": "This is a title",
+          "msgid_plural": null
+        }, {
+          "msgid": "This is a fixed sentence",
+          "msgid_plural": null
+        }, {
+          "msgid": "This is wrapped in single quotes",
+          "msgid_plural": null
+        }, {
+          "msgid": "Image description",
+          "msgid_plural": null
+        }, {
+          "msgid": "Multiline\ntext",
+          "msgid_plural": null
+        }, {
+          "msgid": "Sign in",
+          "msgid_plural": null
+        }, {
+          "msgid": "inside block",
+          "msgid_plural": null
+        }, {
+          "msgid": "inside block inverse",
+          "msgid_plural": null
+        }]
+
+        for(var i=0; i<expected.length; i++) {
+          assert.equal(result[i].msgid, expected[i].msgid)
+          assert.equal(result[i].msgid_plural, expected[i].msgid_plural)
+        }
+        done()
+      })
+    })
+
+    it('should return plural results', function (done) {
+      fs.readFile(__dirname + '/fixtures/plural.hbs', { encoding: 'utf8' }, function (err, data) {
+        if (err) {
+          throw err
+        }
+
+        var result = (new Parser()).parseToPoMessages(data)
+
+        var expected = [{
+          "msgid": "default",
+          "msgid_plural": "defaults"
+        }, {
+          "msgid": "quote",
+          "msgid_plural": "quotes"
+        }]
+        
+        for(var i=0; i<expected.length; i++) {
+          assert.equal(result[i].msgid, expected[i].msgid)
+          assert.equal(result[i].msgid_plural, expected[i].msgid_plural)
+        }
+
+        done()
+      })
+    })
+
+    it('should return results with source path', function (done) {
+      fs.readFile(__dirname + '/fixtures/plural.hbs', { encoding: 'utf8' }, function (err, data) {
+        if (err) {
+          throw err
+        }
+
+        var result = (new Parser()).parseToPoMessages(data, {sourcePath: '/my/file/path.hbs'})
+
+        var expected = [{
+          "msgid": "default",
+          "msgid_plural": "defaults",
+          "references": ["/my/file/path.hbs:1"]
+        }, {
+          "msgid": "quote",
+          "msgid_plural": "quotes",
+          "references": ["/my/file/path.hbs:2"]
+        }]
+        
+        for(var i=0; i<expected.length; i++) {
+          assert.equal(result[i].msgid, expected[i].msgid)
+          assert.equal(result[i].msgid_plural, expected[i].msgid_plural)
+          assert.equal(result[i].references[0], expected[i].references[0])
+        }
+
+        done()
+      })
+    })
+
+    it('should parse files via glob patterns', function (done) {
+      (new Parser()).parseFilesGlob(__dirname + '/fixtures/plural*.hbs', {}, function(er, result) {
+
+        var expected = [{
+          "msgid": "default",
+          "msgid_plural": "defaults",
+          "references": [
+            __dirname + '/fixtures/plural-overlap.hbs:1',
+            __dirname + '/fixtures/plural.hbs:1'
+          ]
+        }, {
+          "msgid": "quote",
+          "msgid_plural": "quotes",
+          "references": [__dirname + '/fixtures/plural.hbs:2']
+        }]
+        
+        for(var i=0; i<expected.length; i++) {
+          assert.equal(result[i].msgid, expected[i].msgid)
+          assert.equal(result[i].msgid_plural, expected[i].msgid_plural)
+
+          for(var n=0; n<expected[i].references.length; n++) {
+            assert.equal(result[i].references[n], expected[i].references[n])
+
+          }
+        }
+        done()
+      })
+    })
+  })
+})
